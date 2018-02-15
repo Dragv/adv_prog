9ccb715a40ddae2590c81aa1f2be6ef4a3ff801c
Alejandro Güereca Valdivia <ale_guereca_valdivia@hotmail.com>
2018-02-14 23:07:12 -0600

    Added first project

---
+++ proyectos/proyecto1/maintest.tcl
@@ -0,0 +1,34 @@
+#!/usr/bin/expect -f
+
+# For colors
+
+proc capability cap {expr {![catch {exec tput -S << $cap}]}}
+
+proc colorterm {} {expr {[capability setaf] && [capability setab]}}
+
+proc tput args {exec tput -S << $args >/dev/tty}
+
+array set color {black 0 red 1 green 2 yellow 3 blue 4 magenta 5 cyan 6 white 7}
+
+proc foreground x {exec tput -S << "setaf $::color($x)" > /dev/tty}
+
+proc background x {exec tput -S << "setab $::color($x)" > /dev/tty}
+
+proc reset {} {exec tput sgr0 > /dev/tty}
+
+
+
+#Test the program
+
+eval spawn [lrange $argv 0 end]
+
+
+#Put your test case here
+
+
+send "3\n
+2 0\n
+3 0\n
+0\r"
+
+expect "010" {foreground green; puts "PASSED";reset} default {foreground red;puts "FAILED";reset}
