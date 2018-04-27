1c12efce02d1cef5134d388f357c6407a1e7ff06
Drag <ale_guereca_valdivia@hotmail.com>
2018-04-26 20:22:50 -0500

    Practica 15
    Signed-off-by: Drag <ale_guereca_valdivia@hotmail.com>

---
+++ practicas/practica15/daemon.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int pid = fork();
+    if(pid == 0){
+        pid = fork();
+        if(pid == 0){
+            fclose(stdout);
+            fclose(stdin);
+            while(1){
+                
+            }
+        }
+    }
+}
---
+++ practicas/practica15/union.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef union i{
+    char parts[4];
+    unsigned dir;
+}IpAddress;
+
+int main(){
+    IpAddress myAddress;
+    myAddress.parts[0] = 127;
+    myAddress.parts[1] = 0;
+    myAddress.parts[2] = 0;
+    myAddress.parts[3] = 1;
+
+    printf("The number is %ud \n", myAddress.dir);
+    myAddress.dir += 1;
+    printf("The %d %d \n", myAddress.parts[0], myAddress.parts[3]);
+    return 0;
+}
